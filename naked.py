881d7e4b5fdcb538e02a7a95e7fffb43a7ea6570
RP <renars.piesis@gmail.com>
2022-09-18 15:11:47 +0000

    Api atslēga

--- naked.py
+++ naked.py
@@ -11,7 +11,7 @@ print('Asteroid processing service')
 print('Loading configuration from file')
 
 # 
-nasa_api_key = "???"
+nasa_api_key = "xl2NTZq6W1AyQcSEAQ0QhPmijVSTyqK3PO4kufA6"
 nasa_api_url = "https://api.nasa.gov/neo/"
 
 # Getting todays date
